e4e1b556733b1a89fa64b240eaa5e0a1128c5a99
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-05 12:39:46 -0300

    email validation

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let container = document.querySelector(".container");
-let sign = document.getElementById("sign-up");
 let btn = document.querySelector(".button-subscribe");
 
-// problema: quando na tela mobile, o design fica diferente
+btn.addEventListener("click", function(e){
+    
+   e.preventDefault(); //parar envio formulário
 
-btn.addEventListener("click", function(){
+    const sign = document.getElementById("sign-up");
+    const container = document.querySelector(".container");
+    const email = document.querySelector("#email");
+    const spanEmail = document.querySelector(".email-entered");
+    let value;
 
-    container.style.display = "none";
-    sign.classList.toggle("hide"); 
+    
+    if (email.value == '' || email.value == 'ash@loremcompany.com') {
+        const pError = document.querySelector('#error')
+
+        email.style.border = '1px solid var(--tomato)';
+        email.style.background = 'hsl(5, 100%, 90%)';
+        email.value = 'ash@loremcompany.com';
+        pError.classList.toggle('hide')
+
+    } else {
+        container.style.display = "none";
+        sign.classList.toggle("hide");
+        
+        value = email.value;
+    
+        spanEmail.innerHTML = value;
+        spanEmail.style.color = '#000';
+    }
 });
